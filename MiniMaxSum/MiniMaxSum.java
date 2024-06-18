2235583f53f93646538140f4be6ab5f99be94dac
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-17 17:41:47 -0700

    revise solution in Java for MiniMaxSum

--- MiniMaxSum/MiniMaxSum.java
+++ MiniMaxSum/MiniMaxSum.java
@@ -21,18 +21,25 @@ class Result {
     public static void miniMaxSum(List<Integer> arr) {
     // Write your code here
     
-    for (int i = 0; i < arr.size(); i++) {
-        // note: since we're working with List<Integer>, we must use .get() method to access elements of a List
-        // useful List methods: .get(), .set()
-        // Resource: List methods: https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/java/util/List.html
-        for (int j = 0; j < arr.size() - 1 - i; j++) {
-            if (arr.get(j) >= arr.get(j + 1)) {
-                int temp = arr.get(j);
-                arr.set(j, arr.get(j + 1));
-                arr.set(j + 1, temp);
-            }
-        }
-    }
+    // Method 1: implementing bubble sort
+    // for (int i = 0; i < arr.size(); i++) {
+    //     // note: since we're working with List<Integer>, we must use .get() method to access elements of a List
+    //     // useful List methods: .get(), .set()
+    //     // Resource: List methods: https://docs.oracle.com/javase%2F8%2Fdocs%2Fapi%2F%2F/java/util/List.html
+    //     for (int j = 0; j < arr.size() - 1 - i; j++) {
+    //         // The condition above (i.e. j < arr.size() - 1 - i) ensures that the index j+1 does not exceed the bounds of the array. 
+    //         // Specifically, when j reaches arr.length - 1 - i, j+1 will be equal to arr.length - i, which is within the bounds of the array. This way, arr[j+1] is always a valid index.
+    //         if (arr.get(j) >= arr.get(j + 1)) {
+    //             int temp = arr.get(j);
+    //             arr.set(j, arr.get(j + 1));
+    //             arr.set(j + 1, temp);
+    //         }
+    //     }
+    // }
+
+    
+    // Method 2: using sort method of Collections
+    Collections.sort(arr);
 
     int min = 0; 
     int max = 0;
