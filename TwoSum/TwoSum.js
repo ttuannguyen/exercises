8ff5eeb3892197c5241f65c45caa9293612bc44b
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-20 14:21:51 -0700

    add files for the Two Sum exercise

---
+++ TwoSum/TwoSum.js
@@ -0,0 +1,14 @@
+const nums = [2,7,11,15];
+const target = 9;
+
+const twoSum = (nums, target) => {
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            // 
+        }
+    }
+    
+}
+
+twoSum(nums, target);
