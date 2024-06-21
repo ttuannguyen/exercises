b7f05080f6d8851c98221ba3d51eb5b520a4c807
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-21 09:41:06 -0700

    add solution to Maximun Array

--- MaximumSubarray/MaximumSubarray.js
+++ MaximumSubarray/MaximumSubarray.js
@@ -1,23 +1,33 @@
 // Objective: Given an integer array nums, find the subarray with the largest sum, and return its sum.
 // Hint: Kadane’s algorithm 
+// Reference: https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 
 const nums = [-2,1,-3,4,-1,2,1,-5,4];
 
 const findMaxSubarray = () => {
-    if (nums.length === 0) return 0;
+    if (nums.length === 0) return [];
 
     let maxSoFar = nums[0];
     let maxEndingHere = nums[0];
+    let start = 0; end = 0; tempStart = 0;
 
     for  (let i = 0; i < nums.length; i++) {
-        maxEndingHere = maxEndingHere + nums[i];
-        if (maxSoFar < maxEndingHere) {
-            maxSoFar = maxEndingHere;
+        if (nums[i] > maxEndingHere + nums[i]) {
+            maxEndingHere = nums[i];
+            tempStart = i;
+        } else {
+            maxEndingHere += nums[i];
         }
-        if (maxEndingHere < 0) {
-            maxEndingHere = 0;
+
+        if (maxEndingHere > maxSoFar) {
+            maxSoFar = maxEndingHere;
+            start = tempStart;
+            end = i;
         }
     }
+
+    return nums.slice(start, end + 1);
 }
 
-findMaxSubarray(nums);
+const result = findMaxSubarray(nums);
+console.log(result);
