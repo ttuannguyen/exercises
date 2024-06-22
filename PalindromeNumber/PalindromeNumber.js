b804d420f26104593913d357046dd2087dbedd6b
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-21 20:37:12 -0700

    add Palindrome Number exercise

---
+++ PalindromeNumber/PalindromeNumber.js
@@ -0,0 +1,15 @@
+// Objective: Given an integer x, return true if x is a palindrome, and false otherwise.
+// Example: Input: x = 121; Output: true; Explanation: 121 reads as 121 from left to right and from right to left.
+
+
+const isPalindromeNumber = (num) => {
+    // const string = num.toString();
+
+    // for (let i = string.length-1; i >= 0; i--) {
+    //     console.log(string[i]);
+    // }
+}
+
+const num = -121;
+
+isPalindromeNumber(num);
