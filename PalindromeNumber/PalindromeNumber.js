ab2cdf468df508ccc056a0f931d87abc2b5d397b
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-21 20:42:57 -0700

    add solution to Palindrome Number

--- PalindromeNumber/PalindromeNumber.js
+++ PalindromeNumber/PalindromeNumber.js
@@ -1,15 +1,22 @@
 // Objective: Given an integer x, return true if x is a palindrome, and false otherwise.
 // Example: Input: x = 121; Output: true; Explanation: 121 reads as 121 from left to right and from right to left.
 
-
 const isPalindromeNumber = (num) => {
-    // const string = num.toString();
+    const string = num.toString();
+    let newString = '';
 
-    // for (let i = string.length-1; i >= 0; i--) {
-    //     console.log(string[i]);
-    // }
+    for (let i = string.length-1; i >= 0; i--) {
+        newString += string[i];
+    }
+
+    if (newString === string) {
+        return true;
+    }
+
+    return false;
 }
 
-const num = -121;
+const num = 121;
 
-isPalindromeNumber(num);
+const answer = isPalindromeNumber(num);
+console.log(answer);
