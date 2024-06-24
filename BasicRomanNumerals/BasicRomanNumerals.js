21ef2c3e013d30c62a912f3b6b4493cee1dd5594
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-24 16:34:44 -0700

    wip solution

--- BasicRomanNumerals/BasicRomanNumerals.js
+++ BasicRomanNumerals/BasicRomanNumerals.js
@@ -1,6 +1,28 @@
-const str = "XLVI";
+const str = "IV";
 
-const convertToInteger = () => {
+const convertToInteger = (str) => {
+    
+    let romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 1000
+    }
+
+    let result = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+        let j = i+1;
+        // console.log(romans[str[i+1]]);
+        if (romans[str[i]] < romans[str[i+1]]) {
+            result = result - romans[str[i]] + romans[str[i+1]]
+        }
+    }
+
+    console.log(result);
+    return result;
 
 }
 
