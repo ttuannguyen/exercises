13508a56e5597acfb04e0685aa82f77388ade563
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-23 19:47:37 -0700

    refined the solution for Remove Duplicates From Sorted Array

--- RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
@@ -1,20 +1,30 @@
-let input = [0,0,1,1,1,2,2,3,3,4]; 
+let nums = [1,1,2]; 
 
-const removeDuplicates = (input) => {
-    // Approach: use a Set to get only the unique values
-    const result = [];
+const removeDuplicates = (nums) => {
+    // Note: we need to maintain the in-place approach; thus, we can't use Set
+    if (nums.length === 0) return 0;
 
-    const seen = new Set();
-    for (let i  = 0; i < input.length; i++) {
-        seen.add(input[i]);
+    let i = 0; // pointer to track the position of the last unique element
+
+    for (let j = 0; j < nums.length; j++) {
+        console.log(nums[j], nums[i])
+        if (nums[j] !== nums[i]) {
+            i++; // increment i to move to the next position for the unique element
+            nums[i] = nums[j]; // update the position i with the new unique element
+        }
     }
+    
+    return i + 1; // since i is intialized to 0, the total number of unique elements is i + 1
+        
+    // Note: if we don't care about in-place positioning, use a Set to get only the unique values
+    // const seen = new Set();
+    // for (let i  = 0; i < nums.length; i++) {
+    //     seen.add(nums[i]);
+    // }
 
-    seen.forEach(num => result.push(num));
-
-    return result;
 
 }
 
-const answer = removeDuplicates(input);
+const answer = removeDuplicates(nums);
 
-console.log(answer);
+// console.log(answer);
