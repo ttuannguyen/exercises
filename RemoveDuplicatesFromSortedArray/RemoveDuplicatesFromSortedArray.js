99bb9efab98a05a457573917a87a801fdb462400
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-23 18:27:58 -0700

    finalized the solution for RemoveDuplicatesFromSortedArray

--- RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
+++ RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
@@ -1,16 +1,18 @@
 let input = [0,0,1,1,1,2,2,3,3,4]; 
 
 const removeDuplicates = (input) => {
-    // Approach: use hash table to tally up the count for each number
-    // Or use a set to get only the unique values
+    // Approach: use a Set to get only the unique values
+    const result = [];
 
     const seen = new Set();
     for (let i  = 0; i < input.length; i++) {
         seen.add(input[i]);
     }
 
-    console.log(seen);
-    
+    seen.forEach(num => result.push(num));
+
+    return result;
+
 }
 
 const answer = removeDuplicates(input);
