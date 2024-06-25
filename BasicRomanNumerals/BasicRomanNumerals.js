b25a924d8e617289f8f55676dc623a8cd3085fd5
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-25 16:27:01 -0700

    add files for the IntegerToRoman exercise

--- BasicRomanNumerals/BasicRomanNumerals.js
+++ BasicRomanNumerals/BasicRomanNumerals.js
@@ -1,10 +1,9 @@
 const str = "XXIV";
-
 // Approach: loop over the string numerals, check if current has a neighbor. 
 // If it does, check if the neighbor's value is more than the current 
     // It it is, we've reached a case where we need to practice subtract notation
 // Else, just add the value of the "current numeral"
-// Challenge: Recognize the subtract notation is the main determinant 
+// Challenge: Recognize that subtract notation is the main determinant 
 
 const BasicRomanNumerals = (str) => {
     let romans = {
@@ -22,7 +21,7 @@ const BasicRomanNumerals = (str) => {
 
     for (let i = 0; i < str.length; i++) {
         let current = str[i];
-        let neighbor = str[i+1]
+        let neighbor = str[i+1];
 
         // check: 'neighbor !== undefined' to ensure we don't access out of bounds of the string.
         if (neighbor !== undefined && romans[neighbor] > romans[current]) {
@@ -32,10 +31,8 @@ const BasicRomanNumerals = (str) => {
         }
         
     }
-
     console.log(total);
     return total;
-
 }
 
 BasicRomanNumerals(str);
---
+++ IntegerToRoman/IntegerToRoman.js
@@ -0,0 +1,7 @@
+const num = 58;
+
+const intToRoman = (num) => {
+    console.log(num);
+}
+
+intToRoman(num);
