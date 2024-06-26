7221623a7dcf58b2b17c15328282c589ce5cae62
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-25 17:55:54 -0700

    add wip solution for Integer To Roman

--- IntegerToRoman/IntegerToRoman.js
+++ IntegerToRoman/IntegerToRoman.js
@@ -1,7 +1,45 @@
-const num = 58;
+const num = 58; // => LVIII
 
 const intToRoman = (num) => {
-    console.log(num);
+
+    let romans = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1,
+    }
+
+    let str = '';
+
+    let text = num.toString();
+
+    if (text[0] !== '4' || text[0] !== '9') {
+        let remainder = num;
+
+        for (let i = 0; i < romans.length-1; i++) {
+            console.log(key, romans[key],remainder, romans[key] - remainder);
+            if((romans[key] - remainder) <= 0) {
+                str += key;
+                remainder = num - romans[key];
+            }
+        }
+
+
+        // for (let key in romans) {
+        //     console.log(key, romans[key],remainder, romans[key] - remainder);
+        //     if((romans[key] - remainder) <= 0) {
+        //         str += key;
+        //         remainder = num - romans[key];
+        //     }
+        // }
+    }
+
+    console.log(str)
+    return str;
+
 }
 
 intToRoman(num);
