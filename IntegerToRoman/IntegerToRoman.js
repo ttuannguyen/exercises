ba01b54f33daaa1be3ce6b9ecb7295183f0d1617
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-26 14:55:22 -0700

    update WIP solution for Integer To Roman

--- IntegerToRoman/IntegerToRoman.js
+++ IntegerToRoman/IntegerToRoman.js
@@ -13,36 +13,34 @@ const intToRoman = (num) => {
     }
 
     let str = '';
-
     let text = num.toString();
+    let remainingValues = 0;
 
     if (text[0] !== '4' || text[0] !== '9') {
         const keys = Object.keys(romans);
         let remainder = num;
-        let lastValue = 0;
         for (let i = 0; i < keys.length-1; i++) {
             console.log(keys[i], romans[keys[i]], romans[keys[i]] - remainder, remainder);
             if((romans[keys[i]] - remainder) <= 0) {
                 if (i === keys.length-2) {
-                    lastValue = -(romans[keys[i]] - remainder);
+                    // To save the remaining value at the last iteration
+                    remainingValues = -(romans[keys[i]] - remainder);
                 }
                 str += keys[i];
                 remainder = num - romans[keys[i]];
             }
             
         }
-
-        console.log(lastValue);
         
-        // for (let key in romans) {
-        //     console.log(key, romans[key],remainder, romans[key] - remainder);
-        //     if((romans[key] - remainder) <= 0) {
-        //         str += key;
-        //         remainder = num - romans[key];
-        //     }
-        // }
-    }
+        for (let i = 0; i < remainingValues; i++) {
+            str += "I";     
+        }
 
+        // const found = Object.keys(romans).find(key => romans[key] === 1);
+    
+
+
+    }
     console.log(str)
     return str;
 
