1a091aebcd4a4b1020748320afa61edfb2934262
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-27 02:08:14 -0700

    revised solution for Evaluate Key Strokes

--- EvaluateKeyStrokes/EvaluateKeyStrokes.js
+++ EvaluateKeyStrokes/EvaluateKeyStrokes.js
@@ -1,23 +1,34 @@
-const input = 'abcde<fg<h'; // => 'abcdfh'
-
 // input = string 
 // remove the character at the position before < and < itself
 
+// Solution 1: Brute force / naive approach: for loop
 const evaluateKeyStrokes = (input) => {
+    let result = [];
 
-    let result = '';
-    
-    // Solution 1: Brute force / naive approach: reverse for loop
-    for (let i = input.length-1; i >= 0; i--) {
-        // if the character is '<', we want to skip 2 places to the front
-        if (input[i] === '<') {
-            i = i - 2;
+    for (let i = 0; i < input.length; i++) {
+        if(result.length > 0 && input[i] === '<') {
+            result.pop();
+        } else {
+            result.push(input[i]);
         }
-        result = `${input[i]}` + result;
     }
 
-
-    return result;
+    // for (let i = input.length-1; i >= 0; i--) {
+    //     // if the character is '<', we want to skip 2 places to the front => flawed
+    //     if (input[i] === '<') {
+    //         i = i - 2;
+    //     }
+    //     result = `${input[i]}` + result;
+    // }
+    
+    return result.join('');
 }
 
+// Solution 2: Using a stack
+const evaluateKeyStrokes2 = (input) => {
+    //
+}
+
+const input = 'abcd<<<fg<h'; // => 'abcdfh'
 let answer = evaluateKeyStrokes(input);
+console.log(answer);
