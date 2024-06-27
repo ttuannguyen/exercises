1059bbe479c24ea5bbfe4b8783aecaf691d29d9f
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-26 23:10:56 -0700

    add solution for Contains Common Item

--- ContainsCommonItem/ContainsCommonItem.js
+++ ContainsCommonItem/ContainsCommonItem.js
@@ -1,7 +1,45 @@
 // Given 2 arrays, create a function that let's a user know (true/false) whether these two arrays contain any common items
 // For Example:
-// const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'i'];
+// const array1 = ['a', 'b', 'c', 'x'];
+//const array2 = ['z', 'y', 'i'];
 // should return false.
 // -----------
-// const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
+// const array1 = ['a', 'b', 'c', 'x'];
+//const array2 = ['z', 'y', 'x'];
 // should return true.
+
+const arr1 = ['a', 'b', 'c', 'x'];
+const arr2 = ['z', 'y', 'x'];
+
+const containsCommonItem = (arr1, arr2) => {
+    // Apprach: 2 separate for loops => time complexity: O(a+b), space complexity: O(a)  
+
+    let map = {};
+
+    // #9 Worth mentioning: modularize code => maybe create a function mayArrayToObject somewhere that does the conversion
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) { // check if the property exists 
+            map[arr1[i]] = true;
+        }
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        // if (map.hasOwnProperty(arr2[i])) {
+        //     return true;
+        // } 
+        if (map[arr2[i]]) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+
+const containsCommonItem2 = (arr1, arr2) => {
+    // Leverage: .some() and .includes()
+    return arr1.some(item => arr2.includes(item))
+    // if some elements inlcude the item in the array, return true
+}
+
+const answer = containsCommonItem2(arr1, arr2);
+console.log(answer);
--- ContainsCommonItem/README.md
+++ ContainsCommonItem/README.md
@@ -5,3 +5,13 @@ should return false.
 -----------
 const array1 = ['a', 'b', 'c', 'x'];//const array2 = ['z', 'y', 'x'];
 should return true.
+
+Step by step
+#3 We might want to ask the interviewer how large this array is going to get; bc if it's just 5 items we might not have to worry about big O or time or space complexity as much. What's more important (efficiency or memory space) 
+#5 & 6 Brute force: nested for loop: O(n^2) quadratic; space complexity O(1)
+#9 This block of code is going to do X and that block of code is going to do Y. Small bits and pieces that read like English. We should write clean code that is readable and easy for anyone to pick up and code along.
+#11 E.g. changing type of an input in an array, if the second array is missing or null
+Can we assume always 2 params? We can implement if statements or check the input etc. 
+Tell the interviewer we're considering these things.
+#13 Test your code: Check for no params, 0, undefined, null, massive arrays, async code, etc... 
+#14 Talk about where we would improve the code. E.g. we can talk about language specific methods that can help streamline the code and improve readability
