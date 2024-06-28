64e6563184fb901812617522f3a905606f01b275
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-27 20:03:40 -0700

    add solution for the Big O Calculation exercise

--- BigO/FindingNemo.js
+++ BigO/FindingNemo.js
@@ -2,19 +2,22 @@
 const fish = ['dory', 'bruce', 'marlin', 'nemo'];
 const nemo = ['nemo'];
 const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
-const large = new Array(10).fill('nemo');
+const large = new Array(100).fill('nemo');
 
 function findNemo2(array) {
   let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
         // uncomment the below to test:
-        // console.log('Found NEMO!');
+        console.log('Found NEMO!');
+        break; // if condition is met, just break out of the loop
+        // Best case: Nemo is at the beginning
+        // Worst case: Nemo is at the end
     }
   }
   let t1 = performance.now();
   // uncomment the below to test:
-  // console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+  console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
 }
 
 findNemo2(large) // O(n) --> linear time; as the # inputs increase, the # operations increases linearly
@@ -23,8 +26,8 @@ findNemo2(large) // O(n) --> linear time; as the # inputs increase, the # operat
 const boxes = [0,1,2,3,4,5];
 
 function logFirstTwoBoxes(boxes) {
-    console.log(boxes[0]); // O(1)
-    console.log(boxes[1]); // O(1)
+    // console.log(boxes[0]); // O(1)
+    // console.log(boxes[1]); // O(1)
 }
 
 logFirstTwoBoxes(boxes); // O(2) => round this down to just O(1)
