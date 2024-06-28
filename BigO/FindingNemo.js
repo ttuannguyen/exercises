1da7e21f13bcb4786426e6c9f73d5590011c6068
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-06-28 10:22:25 -0700

    add examples for Big O

--- BigO/FindingNemo.js
+++ BigO/FindingNemo.js
@@ -33,4 +33,38 @@ function logFirstTwoBoxes(boxes) {
 logFirstTwoBoxes(boxes); // O(2) => round this down to just O(1)
 // very scalable; it doesn't matter how many elements we have, it's going to run the same
 
-// Example #3: Rule #3
+// Example #3: Rule #3 Different terms for inputs
+// Just bc we're seeing 2 for loops, doesn't mean they're looping over the same item
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(function(boxes) { // O(n)
+        console.log(boxes);
+    })
+
+    boxes2.forEach(function(boxes) { // O(n)
+        console.log(boxes);
+    })
+}
+// Result: O(a + b) or O(n + n) => O(2n) => O(n)
+
+// Example #4: when dealing with nested loops, you multiple =>  O(n^2) i.e. quadratic time 
+
+// Example 5: Drop non dominants
+function printAllNumbersThenAllPairSums(numbers) {
+    console.log("these are the numbers:");
+    numbers.forEach(function(number) { // O(n)
+      console.log(number);
+    });
+  
+    console.log("and these are their sums:");
+    numbers.forEach(function(firstNumber) { // O(n^2) bc nested loop
+      numbers.forEach(function(secondNumber) {
+        console.log(firstNumber + secondNumber);
+      });
+    });
+  }
+
+  printAllNumbersThenAllPairSums([1, 2, 3, 4, 5]); // O(n^2)
+  // Result: O(n + n^2) => dropping non dominants => O(n^2)
+  // E.g.) O(x^2+3x+100+x/2) => O(x^2)
+
+  // Note: worst one of all time O(n!)
