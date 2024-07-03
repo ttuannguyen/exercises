16cc3f9711e57cef3afe95fe45ce70b705ce1dd2
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-02 17:21:44 -0700

    add notes re: basic set up and example of reversing an array using 2 pointers method

--- TwoPointerMethod/README.md
+++ TwoPointerMethod/README.md
@@ -1,2 +1,8 @@
 Algorithmic technique involving linear data structure such as array or string
 Using 2 pointers to traverse through the data structure in different ways, typically from different positions (e.g. beginning and end) and moving towards each other or in coordinated manner to achieve a goal
+
+Structure: 
+Sort the array first if not already sorted
+1) Set up the 2 pointers (typically at opposite ends)
+2) Loop and perform some operation at each iteration
+3) Move the pointers to the center (e.g. i++ => move i to the right, j-- => move j to the left)
--- TwoPointerMethod/TwoPointerExamples.js
+++ TwoPointerMethod/TwoPointerExamples.js
@@ -1,6 +1,37 @@
+// Note: This technique works efficiently with sorted arrays
+// So in JS, we can use the .sort() method and execute the 2-pointer technique
+// This technique is used in binary search
+
+// Basic traversal set up
+function twoPointerTraverse(arr) {
+    // Goal: reverse the array, see operation part below
+    let i = 0;
+    let j = arr.length-1;
+
+    while (i < j) {
+        // Log the values of arr[i] and arr[j] before swapping
+        console.log(`Before swap: arr[${i}] = ${arr[i]}, arr[${j}] = ${arr[j]}`);
+
+        // operation at each iteration 
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+        // Log the values of arr[i] and arr[j] after swapping
+        console.log(`After swap: arr[${i}] = ${arr[i]}, arr[${j}] = ${arr[j]}`);
+
+        i++; // increasing left, moving towards the right
+        j--; // decreasing right, moving towards the left
+    }
+
+    console.log(arr);
+} 
+
+
 // Example: TwoSum using pointer method
 const twoSum = (arr, target) =>{
-    // set up at 2 different ends for this purpose
+    // set up the pointers at 2 opposite ends
+    // direction: traversing from 2 ends to the center
     let left = 0;
     let right = arr.length - 1;
 
@@ -9,18 +40,21 @@ const twoSum = (arr, target) =>{
         if (sum === target) {
             return [arr[left], arr[right]];
         } else if (sum < target) {
-            left++;
+            left++; // increasing left, moving towards the right
         } else {
-            right--;
+            right--; // decreasing right, moving towards the left
         }
     }
 
-    return null; // if not such pair exists
+    return null; // if no such pair exists
 }
 
-// Note: input is sorted; 2 pointer works efficiently 
-const arr = [1, 2, 3, 4, 6, 8, 9];
+// Note: input is sorted
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const target = 10;
 
-let answer = twoSum(arr, target);
-console.log(answer);
+
+ twoPointerTraverse(arr);
+
+// let answer = twoSum(arr, target);
+// console.log(answer);
