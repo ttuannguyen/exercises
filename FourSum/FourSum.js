33e397955e1bb81e3dd5cf8279ba74d1aec7d50f
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-03 16:55:31 -0700

    add solution for Four Sum

--- FourSum/FourSum.js
+++ FourSum/FourSum.js
@@ -1,11 +1,27 @@
 function fourSum(arr, target) {
     arr.sort((a, b) => a - b);
 
-    for (let i = 0; i < arr.length-3; i++) {
-        
+    for (let i = 0; i < arr.length - 3; i++) {
+        for (let j = i + 1; j < arr.length - 2; j++) {
+            let complement = target - arr[i] - arr[j];
+
+            let left = i + 1;
+            let right = arr.length-1;
+
+            while (left < right) {
+                if (arr[left] + arr[right] === complement) {
+                    return [arr[i], arr[j], arr[left], arr[right]];
+                } else if (arr[left] + arr[right] < complement) {
+                    left++;
+                } else {
+                    right--;
+                }
+            } 
+        }
     }
+    return null;
 }
 
 const arr = [2, 7, 4, 0, 9, 5, 1, 3];
 const target = 20;
-fourSum(arr, target);
+console.log(fourSum(arr, target));
