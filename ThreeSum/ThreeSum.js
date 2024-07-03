36220cac1c7c60a881e52baf40ea80c57daa0e57
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-03 14:03:06 -0700

    add solution for ThreeSum

--- ThreeSum/ThreeSum.js
+++ ThreeSum/ThreeSum.js
@@ -1,8 +1,27 @@
-const findThreeSum = (array, target) => {
-    //
+const threeSum = (array, target) => {
+    let result = [];
+    array.sort((a, b) => a - b);
+
+    for (let i = 0; i < array.length; i++) {
+        let complement = target - array[i];
+        console.log(complement);
+        
+        let j = i + 1;
+        let k = array.length - i - 1;
+
+        while (j < k) {
+            if (array[j] + array[k] === complement) {
+                result.push(array[i], array[j], array[k]);
+                return result;
+            }
+            j++;
+            k--;
+        }
+    }
+    return null;
 }
 
 // Example usage
 const array = [1, 4, 6, 2, 3];
-const target = 10;
-console.log(threeSum(array, target)); // Output: [1, 4, 5]
+const target = 9;
+console.log(threeSum(array, target)); // Output: [1, 2, 6]
