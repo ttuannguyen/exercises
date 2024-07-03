cb2d80657c5da7cf59144b6935c1838e8261b81d
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-03 14:41:13 -0700

    add WIP solution for Four Sum

---
+++ FourSum/FourSum.js
@@ -0,0 +1,11 @@
+function fourSum(arr, target) {
+    arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length-3; i++) {
+        
+    }
+}
+
+const arr = [2, 7, 4, 0, 9, 5, 1, 3];
+const target = 20;
+fourSum(arr, target);
