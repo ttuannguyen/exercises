970001fa0295bbb3469ae50f0f7f8def685a08d4
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-03 19:39:40 -0700

    add notes for time and space complexity for Schedule Conflicts

--- ScheduleConflicts/ScheduleConflicts.js
+++ ScheduleConflicts/ScheduleConflicts.js
@@ -1,21 +1,24 @@
 function findScheduleConflicts(times) {
     // Leverage built-in sort method of JavaScript
-    times.sort((a, b) => a[0] - b[0])
-    const conflicts = new Set();
+    times.sort((a, b) => a[0] - b[0]) // O(n log n)
+    const conflicts = new Set(); 
 
     // Implement a comparison 
-    for (let i = 0; i < times.length - 1; i++) {
+    for (let i = 0; i < times.length - 1; i++) { // O(n)
         let a = times[i];
         let b = times[i+1];
 
         if (b[0] < a[1]) {
-            conflicts.add(a);
-            conflicts.add(b);
+            conflicts.add(a); // O(1)
+            conflicts.add(b); // O(1)
         }
     }
     
-    const result = conflicts.values();
+    const result = conflicts.values(); // // O(n)
     return result;
+
+    // Time complexity: O(nlogn)+O(n)+O(n)=O(nlogn)
+    // Space complexity: O(n)
     
 }
 
