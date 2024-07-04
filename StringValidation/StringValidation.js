38ef2b39cf322b0a7489b34c8993f78e9d9358e8
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-04 11:17:46 -0700

    add solution for String Validation

--- StringValidation/StringValidation.js
+++ StringValidation/StringValidation.js
@@ -3,30 +3,35 @@
 // loop through the array and verify that each number is in the range: 0 - 255
 
 function validateIPAddress(ip) {
-    let arr = [];
-    let str = '';
+    let stringArray = ip.split(".");
+    let numArray = stringArray.map(Number); // O(n)
 
-    // let i = 0;
-    // let n = ip.length -1;
-    // while (i < n) {
-    //     //
-    // }
-
-    for (let i = 0; i < ip.length - 1; i++) {
-        if(ip[i] !== '.') {
-            str += ip[i];
-        } else {
-            arr.push(parseInt(str));
-            str = '';
+    for (let i = 0; i < numArray.length; i++) { // O(n)
+        if(!(numArray[i] >= 0 && numArray[i] <= 255)) {
+            return false; 
         }
     }
 
-    // we need to capture reminder of string
+    return true;
 
-    console.log(arr);
+    // Approach: Manually split the string using for loop
+    // let arr = [];
+    // let str = '';
+    // for (let i = 0; i < ip.length; i++) {
+    //     if(ip[i] !== '.') {
+    //         str += ip[i];
+    //     } else {
+    //         arr.push(parseInt(str));
+    //         str = ''; 
+    //         // this resets string but will the the loop will still go on and the above "if" condition will still capture characters
+    //         // at the end of of the loop, str will hold the rest of the characters
+    //     }
+    // }
+    // // capture reminder of string in arr
+    // arr.push(parseInt(str));
 
 }
 
 
-const ip = '192.168.0.1';
-validateIPAddress(ip);
+const ip = '192.168.123.456';
+console.log(validateIPAddress(ip));
