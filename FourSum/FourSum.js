e3d59c82c33bbb45e262c5fc720ee8e4bb556784
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-03 18:25:02 -0700

    add notes for the hash map approach to solve Four Sum

--- FourSum/FourSum.js
+++ FourSum/FourSum.js
@@ -1,3 +1,5 @@
+// Approach 1: Using nested loops and 2-pointer technique
+// Time complexity: O(n^3)
 function fourSum(arr, target) {
     arr.sort((a, b) => a - b);
 
@@ -20,6 +22,20 @@ function fourSum(arr, target) {
         }
     }
     return null;
+}
+
+// TODO: Approach 2: Using hash map 
+// Sort the array
+// Create a hash map: set sums as keys and pairs (e.g. [a, b]) as values
+// Iterate through the array to find quadruplets using a nested loop (2 for loops):
+    // Complement = total - sum of 2 pairs
+    // If hash map has the complement
+    // If yes, iterate through the hash map and retrieve the list of pairs in the hash map
+    // Deconstruct that pair into indices
+    // Put a condition to ensure indices are in increasing order to avoid overlap
+
+function fourSum2(arr, target) {
+
 }
 
 const arr = [2, 7, 4, 0, 9, 5, 1, 3];
