ce17dac9393d43b830b77300d273cac4db029b6c
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-05 19:18:40 -0700

    add edge cases for Find Lower Bound

--- BinarySearch/FindLowerBound.js
+++ BinarySearch/FindLowerBound.js
@@ -5,9 +5,22 @@ Hint: consider loop invariants array[lo] < X and array[hi] >= X
 */
 
 function lowerBound(array, x) {
+    let n = array.length;
+    
+    if (array[n] < x) {
+        return -1;
+    }
+
+    if (array[0] >=  x) {
+        return 0;
+    }
+
+
     console.log(array, x);
 }
 
 
 const array = [1, 2, 4, 6, 8];
 const x = 5;
+let answer = lowerBound(array, x);
+console.log(answer);
