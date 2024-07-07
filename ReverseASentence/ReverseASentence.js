6847c0f55e4dfd2ba1435aadd871e08d3c4d10a6
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-07 14:20:05 -0700

    add a file for Reverse A String exercise

--- ReverseASentence/ReverseASentence.js
+++ ReverseASentence/ReverseASentence.js
@@ -1,12 +1,58 @@
 // Empty spaces separate words
-// Note:
-// We'll need a helper function to help reverse the whole arr as well as part of the arr; it's 2-pointer yay!
 
+// Approach: Using a stack
+// We can use .push() and .pop() method of JavaScript
+function reverseWords2(arr) {
+    let stack = [];
+    let word = [];
+    let space = [];
+
+    // Traverse the array to extract words and spaces and push them onto the stack
+    for (let char of arr) {
+        if (char !== ' ') {
+            if (space.length > 0) {
+                stack.push(space.join(''));
+                space = [];
+            }
+            word.push(char);
+        } else {
+            if (word.length > 0) {
+                stack.push(word.join(''));
+                word = [];
+            }
+            space.push(char);
+        }
+    }
+    if (word.length > 0) {
+        stack.push(word.join(''));
+    }
+    if (space.length > 0) {
+        stack.push(space.join(''));
+    }
+
+    let result = [];
+    
+    // Pop elements from the stack to build the reversed array
+    while (stack.length > 0) {
+        let element = stack.pop();
+        for (let char of element) {
+            result.push(char);
+        }
+    }
+
+    return result;
+    
+}
+
+
+
+// Approach: 2-pointer technique
+// We'll need a helper function to help reverse the whole arr as well as part of the arr; it's 2-pointer yay!
 function reverseWords(arr) {
     
     // Reverse the whole array 
     reverseHelper(arr, 0, arr.length-1)
-    console.log(arr);
+    console.log(arr)
 
     // Reverse each word in the reversed array
     let start = 0;
@@ -23,7 +69,7 @@ function reverseWords(arr) {
     // let start = 0; 
     // let end = -1;
     // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] !== '  ') {
+    //     if (arr[i] !== ' ') {
     //         end++
     //     } else {
     //         // console.log(start, end);
@@ -55,7 +101,8 @@ const arr = [ 'p', 'e', 'r', 'f', 'e', 'c', 't', '  ',
             'm', 'a', 'k', 'e', 's', '  ',
             'p', 'r', 'a', 'c', 't', 'i', 'c', 'e' ];
   
-console.log(reverseWords(arr));
+let answer = reverseWords(arr);
+console.log(answer);
 
 
 
---
+++ ReverseAString/ReverseAString.js
@@ -0,0 +1,14 @@
+function reverse(str) {
+    let stack = [];
+
+    for (const char in str) {
+        //
+    }
+
+    
+}
+
+reverse('hello');
+
+
+
