15130c9a1ff0303966d0159094eabdfd618df483
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-09 11:32:44 -0700

    add files for the Most Frequent Non-Banned Word exercise

---
+++ MostFreqNonBannedWord/Solution.js
@@ -0,0 +1,9 @@
+function find(paragraph, banned) {
+    
+    let strArray = paragraph.split();
+    console.log(strArray);
+    
+}
+
+const paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
+const banned = "hit";
