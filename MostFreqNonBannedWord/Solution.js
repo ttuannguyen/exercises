cd169788a8684903a4a975361fe6372023c2e2ee
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-09 12:13:21 -0700

    add solution for Most Freq Non-Banned Word

--- MostFreqNonBannedWord/Solution.js
+++ MostFreqNonBannedWord/Solution.js
@@ -1,9 +1,34 @@
 function find(paragraph, banned) {
     
-    let strArray = paragraph.split();
-    console.log(strArray);
-    
+    let regExp = /[^a-z]/g;
+    let regExp2 = /[!?',;.]/g;
+    let sanitized = paragraph.toLowerCase().replace(regExp2, '');
+    let strArray = sanitized.split(/\s+/);
+    // console.log(strArray);
+
+    let map = {};
+    let highestCount = 0;
+    let mostFrequentWord = '';
+
+    for (let i = 0; i < strArray.length; i++) {
+        if (strArray[i] !== banned) {
+            if(map[strArray[i]] !== undefined) {
+                map[strArray[i]]++;
+            } else {
+                map[strArray[i]] = 1;
+            }
+            if (map[strArray[i]] > highestCount) {
+                highestCount = map[strArray[i]];
+                mostFrequentWord = strArray[i];
+            }
+        }
+    }
+
+    console.log(mostFrequentWord);
+    return mostFrequentWord;
 }
 
 const paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
 const banned = "hit";
+
+find(paragraph, banned);
