8bc74b8cb267e02472b47b8fe6e55a4e5ce2a8e3
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-09 19:01:25 -0700

    add another solution using only 1 for loop to Busiest Time in Mall

--- BusiestTimeInMall/Solution.js
+++ BusiestTimeInMall/Solution.js
@@ -1,9 +1,10 @@
 // Clarification: The main determinant is the total visitors present during that time, which can be accumulated from the last time
-function findBusiestPeriod(data) {
+
+// Approach 1: Using a hash table
+function findBusiestPeriod1(data) {
     // your code goes here
     let map = {};
     let peopleCount = 0;
-    let time = '';
   
     for (let i = 0; i < data.length; i++) {
         if (data[i][2] === 1) {
@@ -11,9 +12,7 @@ function findBusiestPeriod(data) {
         } else {
             peopleCount -= data[i][1];  
         }
-
         map[data[i][0]] = peopleCount;
-
     }
     
     let max = 0;
@@ -27,9 +26,35 @@ function findBusiestPeriod(data) {
 
     console.log(maxKey);
     return parseInt(maxKey);
+}
+
+// Approach 2: Only using 1 for loop
+function findBusiestPeriod2(data) {
+    let currentVisitors = 0;
+    let maxVisitors = 0;
+    let busiestTime = 0;
+
+    for (let i = 0; i < data.length; i++) {
+        const [timestamp, count, type] = data[i];
+
+        if (type === 1) {
+            currentVisitors += count; // people entering
+        } else {
+            currentVisitors -= count; // people existing
+        }
+
+        // If next timestamp is different or we're at the end of the array => ready for comparison
+        if (i === data.length -1 || data[i + 1][0] !== timestamp) {
+            if (currentVisitors > maxVisitors) { 
+                maxVisitors = currentVisitors;
+                busiestTime = timestamp;
+            }
+        }
+    }
+
+    return busiestTime;
 
 }
-  
   
 const data =[[1487799425, 14, 1], 
             [1487799425, 4, 1], 
