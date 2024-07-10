aa9474cc7a4714cb7346bd07ac5b49839b998edf
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-09 18:37:55 -0700

    add revised solution for Busiest Time In Mall

--- BusiestTimeInMall/Solution.js
+++ BusiestTimeInMall/Solution.js
@@ -2,42 +2,33 @@
 function findBusiestPeriod(data) {
     // your code goes here
     let map = {};
-    let highestPeopleCount = 0;
+    let peopleCount = 0;
     let time = '';
   
     for (let i = 0; i < data.length; i++) {
-      if(map[data[i][0]] !== undefined) {
-          if (data[i][2] === 1) {
-              map[data[i][0]] = map[data[i][0]] + data[i][1];  
-          } else {
-              map[data[i][0]] = map[data[i][0]] - data[i][1];  
-          }
-      } else {
-          if (data[i][2] === 1) {
-              map[data[i][0]] = data[i][1];  
-          } else {
-              map[data[i][0]] = -(data[i][1]);  
-          }
-      }
+        if (data[i][2] === 1) {
+            peopleCount += data[i][1];  
+        } else {
+            peopleCount -= data[i][1];  
+        }
+
+        map[data[i][0]] = peopleCount;
+
+    }
+    
+    let max = 0;
+    let maxKey = '';
+    for (let key in map) {
+        if(map[key] > max) {
+            max = map[key];
+            maxKey = key;
+        }
     }
 
-    // const maxKey = Object.entries(map).reduce((maxKey, [key, value]) => {
-    //     return value > map[maxKey] ? key : maxKey;
-    // }, Object.keys(map)[0]); 
+    console.log(maxKey);
+    return parseInt(maxKey);
 
-    // let max = 0;
-    // let maxKey = '';
-    // for (let key in map) {
-    //     if(map[key] > max) {
-    //         max = map[key];
-    //         maxKey = key;
-    //     }
-    // }
-
-    // console.log(map);
-    // console.log(maxKey);
-    // return parseInt(maxKey);
-  }
+}
   
   
 const data =[[1487799425, 14, 1], 
