33dad17a4a2e28b57fae7f54b3f137cfc8bed133
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-10 20:28:32 -0700

    add WIP solution for Product Of Array Except Self

--- ProductOfArrayExceptSelf/Solution.js
+++ ProductOfArrayExceptSelf/Solution.js
@@ -1,5 +1,37 @@
-// Approach: brute force
-function productExceptSelf(nums) {
+// Approach 3: 2 passes (using 2 separate for loops)
+function productExceptSelf2(nums) {
+    if (nums.length === 0) {
+        return [];
+    }
+}
+
+// Approach 2: Flawed bc we need to deal with running into division by 0
+function productExceptSelf2(nums) {
+    // Aha: The key here is to recognize the mathematical patterns 
+    if (nums.length === 0) {
+        return [];
+    }
+
+    let n = nums.length;
+
+    let arr = [];
+
+    let product = 1;
+    for (let i = 0; i < n; i++) {
+        product = product * nums[i];
+    };
+
+    for (let i = 0; i < n; i++) {
+        arr.push(product / nums[i]);
+    }
+
+    return arr;
+
+}
+
+
+// Approach (brute force): Nested for loop
+function productExceptSelf1(nums) {
     if (nums.length === 0) {
         return [];
     }
@@ -17,10 +49,9 @@ function productExceptSelf(nums) {
         result.push(product);
     }
 
-    console.log(result);
     return result;
-
 }
 
 // debug your code below
-console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(productExceptSelf1([1, 2, 3, 4]));
+console.log(productExceptSelf2([1, 2, 3, 4]));
