c1cde75e314f36f4c57e9f37134f7e5e3c660b99
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-10 18:38:44 -0700

    add brute force solution for Product Of Array Except Self

--- ProductOfArrayExceptSelf/Solution.js
+++ ProductOfArrayExceptSelf/Solution.js
@@ -1,9 +1,24 @@
+// Approach: brute force
 function productExceptSelf(nums) {
-    // your code goes here
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++)
-        console.log(i, j)
+    if (nums.length === 0) {
+        return [];
     }
+    
+    let result = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        let product = 1;
+
+        for (let j = 0; j < nums.length; j++) {
+            if (j !== i) {
+                product = product * nums[j];
+            }
+        }
+        result.push(product);
+    }
+
+    console.log(result);
+    return result;
 
 }
 
