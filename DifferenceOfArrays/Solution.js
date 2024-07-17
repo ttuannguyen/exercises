b2cb7624c7ba6da7add3119bcb9d0fd00e88316f
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-16 19:18:47 -0700

    add another solution for Difference Of Arrays

--- DifferenceOfArrays/Solution.js
+++ DifferenceOfArrays/Solution.js
@@ -1,9 +1,31 @@
-// Brute force:
+// Approach: Leverage .includes method in JS
+function diff2(arrA, arrB) {
+    let result = [];
+
+    // Check if each item in arrA is not in arrB
+    for (let i = 0; i < arrA.length; i++) {
+        if (!arrB.includes(arrA[i])) {
+            result.push(arrA[i]);
+        }
+    }
+
+    // Check if each item in arrB is not in arrA
+    for (let i = 0; i < arrB.length; i++) {
+        if (!arrA.includes(arrB[i])) {
+            result.push(arrB[i]);
+        }
+    }
+
+    return(result);
+
+}
+
+
+// Approach: Brute force
 // Build a hash table
 // 1st pass: iterate through the 1st array and populate map with initial numbers
 // 2nd pass: create a set from the 2nd array, iterate through it and market the duplicates
-// build a result array based on the table 
-function diff(arrA, arrB) {
+function diff1(arrA, arrB) {
     // your code goes here
     let map = {};
 
@@ -34,4 +56,4 @@ function diff(arrA, arrB) {
 }
 
 // debug your code below
-console.log(diff([1, 2, 3, 4], [3, 4, 5, 6]));
+console.log(diff2([1, 2, 3, 4], [3, 4, 5, 6]));
