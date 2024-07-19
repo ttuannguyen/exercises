bcf3defd3f3c1f46fd102acfa45e2acb9745da6d
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-19 00:11:35 -0700

    add solution for Most Common Words

--- MostCommonWords/Solution.js
+++ MostCommonWords/Solution.js
@@ -1,5 +1,25 @@
 function mostCommonWords(text) {
     // Your code here
+    let sanitized = text.toLowerCase().replace(/[!?',;.]/g, '');
+    let words = sanitized.split(/\s+/);
+
+    let map = {};
+
+    for (let i = 0; i < words.length; i++) {
+        if (map[words[i]] !== undefined) {
+            map[words[i]]++;
+        } else {
+            map[words[i]] = 1;
+        }
+    }
+    
+    let arr = [];
+    for (const word in map) {
+        arr.push([word, map[word]]);
+    }
+
+    // Challenge: sort 
+
   }
   
   // debug your code below
