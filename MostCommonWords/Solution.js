68f50a62873a8f54e78382834814bfbea4cb2a71
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-21 20:37:31 -0700

    add another solution using Map object for Most Common Words

--- MostCommonWords/Solution.js
+++ MostCommonWords/Solution.js
@@ -1,3 +1,31 @@
+// Approach: 
+function mostCommonWords2(text) {
+    // Your code here
+    let sanitized = text.toLowerCase().replace(/[!?',;.]/g, '');
+    let words = sanitized.split(/\s+/);
+    console.log(words);
+
+    let map = new Map(); 
+
+    for (const word of words) {
+        if(word !== '') {
+            map.set(word, (map.get(word) || 0), + 1);
+        }
+    }
+
+    const punctuationRe = /[.,;!'"()]/g;
+    const sanitizedText = text.replace(punctuationRe, ' ').toLowerCase();
+    const wordArray = sanitizedText.split(/\s+/);
+    console.log(wordArray);
+
+
+
+    
+
+  }
+
+
+  // Approach: brute force / incomplete
 function mostCommonWords(text) {
     // Your code here
     let sanitized = text.toLowerCase().replace(/[!?',;.]/g, '');
@@ -18,9 +46,12 @@ function mostCommonWords(text) {
         arr.push([word, map[word]]);
     }
 
-    // Challenge: sort 
+    let sortedArr = arr.sort();
+    console.log(sortedArr);
+
+    // Challenge: sort alphabetically and t
 
   }
   
   // debug your code below
-  console.log(mostCommonWords("It was the best of times, it was the worst of times."));
+  console.log(mostCommonWords2("It was the best of times, it was the worst of times."));
