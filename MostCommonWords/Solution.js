8ed9cd8df062a067835d40e0e8d754c1802ac3f0
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-21 22:26:32 -0700

    update the solution using Map object for Most Common Words

--- MostCommonWords/Solution.js
+++ MostCommonWords/Solution.js
@@ -1,28 +1,30 @@
 // Approach: 
 function mostCommonWords2(text) {
     // Your code here
-    let sanitized = text.toLowerCase().replace(/[!?',;.]/g, '');
+    let sanitized = text.toLowerCase().replace(/[.,;!'"()]/g, '');
     let words = sanitized.split(/\s+/);
     console.log(words);
 
-    let map = new Map(); 
+    let wordFreq = new Map(); 
 
     for (const word of words) {
         if(word !== '') {
-            map.set(word, (map.get(word) || 0), + 1);
+            wordFreq.set(word, (wordFreq.get(word) || 0) + 1);
         }
     }
 
-    const punctuationRe = /[.,;!'"()]/g;
-    const sanitizedText = text.replace(punctuationRe, ' ').toLowerCase();
-    const wordArray = sanitizedText.split(/\s+/);
-    console.log(wordArray);
+    const sortedWords = Array.from(wordFreq.entries()).sort((a, b) => {
+        if (b[1] !== a[1]) { // comparing frequencies of the two words
+            return b[1] - a[1];
+        } else { // if the frequencies are the same, compare the words alphabetically
+            // localeCompare is a string method that compares two strings according to the sort order of the current locale.
+            return a[0].localeCompare(b[0]);
+        }
+    })
 
+    console.log(sortedWords);
 
-
-    
-
-  }
+}
 
 
   // Approach: brute force / incomplete
