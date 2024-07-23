017d18c8dc3ff93812c9a004cd22b62d998da28c
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-22 23:50:07 -0700

    add brute force solution for Smaller Missing Number

--- SmallestMissingInteger/Solution.js
+++ SmallestMissingInteger/Solution.js
@@ -1,7 +1,21 @@
-// Approach: 
+// Approach: Brute force
 function getDifferentNumber(arr) {
-    // your code goes here
-  }
+    let sortedArr = arr.sort();
+    console.log(sortedArr);
+    
+    // edge
+    if (sortedArr[0] !== 0) return 0;
+    if (sortedArr[1] !== 1) return 1;
+
+
+    for (let i = 0; i < sortedArr.length; i++) {
+        // console.log(sortedArr[i]);
+        if (arr[i] !== i) {
+            return i;
+        }
+    }
+    
+}
   
 // debug your code below
 const arr = [0, 2, 1, 3, 5];
