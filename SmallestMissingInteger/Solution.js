529dbbee9f418d6d7110ec982bbb9e045ce36366
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-24 14:35:28 -0700

    add solution using Set for Smallest Missing Integer

--- SmallestMissingInteger/Solution.js
+++ SmallestMissingInteger/Solution.js
@@ -1,11 +1,18 @@
-// Approach: 
-function getDifferentNumber(arr) {
-    //
+// Approach: Using a Set
+function getDifferentNumber2(arr) {
+    let set = new Set(arr);
+    for (let i = 0; i <= arr.length; i++) { // i <= arr.length bc ...
+        if (set.has(i)) {
+            console.log(i, " yes");
+        } else {
+            console.log(i, " no");
+        }
+    }
 }
 
 // Approach: Brute force
-function getDifferentNumber(arr) {
-    let sortedArr = arr.sort();
+function getDifferentNumber1(arr) {
+    let sortedArr = arr.sort(); // O (n*log(n))
     console.log(sortedArr);
     
     // edge
@@ -13,17 +20,15 @@ function getDifferentNumber(arr) {
     if (sortedArr[1] !== 1) return 1;
 
 
-    for (let i = 0; i < sortedArr.length; i++) {
+    for (let i = 0; i <= sortedArr.length; i++) {
         // console.log(sortedArr[i]);
         if (arr[i] !== i) {
             return i;
         }
     }
-
     return arr.length;
-    
 }
   
 // debug your code below
 const arr = [0, 2, 1, 3, 5];
-console.log(getDifferentNumber(arr));
+console.log(getDifferentNumber2(arr));
