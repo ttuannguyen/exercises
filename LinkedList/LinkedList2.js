3191dd14011dde66cc9832d3700e2e8dd9743107
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-25 22:12:05 -0700

    build the append() method for a linked list

--- LinkedList/LinkedList2.js
+++ LinkedList/LinkedList2.js
@@ -1,9 +1,28 @@
-class LinedList {
+class LinkedList {
     constructor(value) {
         this.head = {
             value: value,
             next: null
-        }
+        };
+        this.tail = this.head;
+        this.length = 1;
     }
-
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
 }
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(14);
+myLinkedList.append(53);
+myLinkedList.append(37);
+console.log(myLinkedList);
+
