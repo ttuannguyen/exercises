49d097534f683f45358eecd64ba07c1d476cd92d
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-26 20:33:57 -0700

    test traversing through a 2D array for Copy A Spiral Matrix

--- CopyASpiralMatrix/Solution.js
+++ CopyASpiralMatrix/Solution.js
@@ -1,11 +1,21 @@
+// Gameplan:
+// Traverse through the 2D array
+// Pattern: going through outer layer first, i.e. first row, last col, last row, first col, and so on 
+
 function spiralCopy(inputMatrix) {
     // your code goes here
-  }
+    for (let i = 0; i < inputMatrix.length; i++) {
+        // console.log(inputMatrix[i]);
+        for (let j = 0; j < inputMatrix[i].length; j++) {
+            console.log(inputMatrix[i][j])
+        }
+    }
+}
   
-  // debug your code below
-  const inputMatrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]]
-  
-  console.log(spiralCopy(inputMatrix))
+// debug your code below
+const inputMatrix = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]]
+
+console.log(spiralCopy(inputMatrix))
