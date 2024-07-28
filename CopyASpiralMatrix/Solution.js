0c9ba45db76dc25d5c154846bff854f296ed5278
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-07-27 19:42:53 -0700

    add solution for the CopyASpiralMatrix exercise

--- CopyASpiralMatrix/Solution.js
+++ CopyASpiralMatrix/Solution.js
@@ -4,25 +4,77 @@
 
 function spiralCopy(inputMatrix) {
     // your code goes here
-    for (let i = 0; i < inputMatrix.length; i++) {
-        // console.log(inputMatrix[i]);
-        for (let j = 0; j < inputMatrix[i].length; j++) {
-            console.log(inputMatrix[i][j])
-        }
-    }
+    // for (let i = 0; i < inputMatrix.length; i++) {
+    //     // console.log(inputMatrix[i]);
+    //     for (let j = 0; j < inputMatrix[i].length; j++) {
+    //         console.log(inputMatrix[i][j])
+    //     }
+    // }
 
     let numRows = inputMatrix.length;
     let numCols = inputMatrix[0].length;
 
+    // starting poiters to traverse
     let topRow = 0;
-    let bottomRow = numRows- 1; 
+    let bottomRow = numRows - 1; 
     let leftCol = 0;
     let rightCol = numCols - 1;
     
-    let result = [];
+    const result = [];
 
-
+    while (leftCol <= rightCol && topRow <= bottomRow) {
+        // Copy the next top row
+        for (let i = leftCol; i <= rightCol; i++) {
+            result.push(inputMatrix[topRow][i]);
+        }
+        topRow++;
+    }
+    return result;
 }
+
+
+function spiralCopy2(inputMatrix) {
+    const numRows = inputMatrix.length;
+    const numCols = inputMatrix[0].length;
+  
+    let topRow = 0;
+    let btmRow = numRows - 1;
+    let leftCol = 0;
+    let rightCol = numCols - 1;
+    const result = [];
+  
+    while (topRow <= btmRow && leftCol <= rightCol) {
+      // Copy the next top row
+      for (let i = leftCol; i <= rightCol; i++) {
+        result.push(inputMatrix[topRow][i]);
+      }
+      topRow++;
+  
+      // Copy the next right hand side column
+      for (let i = topRow; i <= btmRow; i++) {
+        result.push(inputMatrix[i][rightCol]);
+      }
+      rightCol--;
+  
+      // Copy the next bottom row
+      if (topRow <= btmRow) {
+        for (let i = rightCol; i >= leftCol; i--) {
+          result.push(inputMatrix[btmRow][i]);
+        }
+        btmRow--;
+      }
+  
+      // Copy the next left hand side column
+      if (leftCol <= rightCol) {
+        for (let i = btmRow; i >= topRow; i--) {
+          result.push(inputMatrix[i][leftCol]);
+        }
+        leftCol++;
+      }
+    }
+  
+    return result;
+  }
   
 // debug your code below
 const inputMatrix = [
@@ -30,4 +82,4 @@ const inputMatrix = [
 [4, 5, 6],
 [7, 8, 9]]
 
-console.log(spiralCopy(inputMatrix))
+spiralCopy2(inputMatrix)
