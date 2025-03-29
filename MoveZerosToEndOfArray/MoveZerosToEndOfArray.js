4dcb12f6aa60a99372986eae347a9c3a20f21703
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-03-28 20:32:58 -0400

    add brute for solution for zeroCounter practice

--- MoveZerosToEndOfArray/MoveZerosToEndOfArray.js
+++ MoveZerosToEndOfArray/MoveZerosToEndOfArray.js
@@ -1,7 +1,25 @@
 function solution (arr) {
-    console.log(arr);
+    let newArr = [];
+    let zeroCounter = 0;
+
+    for (let i = 0; i < arr.length; i ++) {
+        if (arr[i] !== 0) {
+            newArr.push(arr[i]);
+        } else {
+            zeroCounter++;
+        }
+    }
+
+    if (zeroCounter > 0) {
+        for (let i = 1; i <= zeroCounter; i++) {
+            newArr.push(0);
+        }
+    }
+
+    return newArr;
 }
 
 const arr = [0, 1, 0, 3, 12];
 
-console.log(solution());
+console.log(solution(arr));
+
