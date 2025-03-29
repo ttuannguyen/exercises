fb4ff684489e440b02a04ee2881f2b506e8c2958
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-03-28 21:47:44 -0400

    implement solution for MoveZerosToEndOfArray.js without making a copy of the array

--- MoveZerosToEndOfArray/MoveZerosToEndOfArray.js
+++ MoveZerosToEndOfArray/MoveZerosToEndOfArray.js
@@ -1,22 +1,14 @@
 function solution (arr) {
-    let newArr = [];
-    let zeroCounter = 0;
+
+    // Useful method: .splice() 
 
     for (let i = 0; i < arr.length; i ++) {
-        if (arr[i] !== 0) {
-            newArr.push(arr[i]);
-        } else {
-            zeroCounter++;
+        if (arr[i] === 0) {
+            arr.splice(i, 1);
+            arr.push(0);
         }
     }
-
-    if (zeroCounter > 0) {
-        for (let i = 1; i <= zeroCounter; i++) {
-            newArr.push(0);
-        }
-    }
-
-    return newArr;
+    return arr;
 }
 
 const arr = [0, 1, 0, 3, 12];
