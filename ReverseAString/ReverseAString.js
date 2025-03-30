2951ff4d0060c19f2c9384d8e920738b9393f5fd
Tuan Nguyen <trentontuannguyen@gmail.com>
2025-03-30 15:02:03 -0400

    add solutuin to ReverseAString practice

--- ReverseAString/ReverseAString.js
+++ ReverseAString/ReverseAString.js
@@ -1,58 +1,32 @@
-// Approach 1: stack data structure
-// Overall time complexity: O(n)
-// Make a stack, use .push(), and .pop() method
-function reverse(str) {
-    // Create a stack in the form of an empty arr
-    let stack = []; // O(1)
+function reverse2(str) {
+    let stack = [];
 
-    // Populate the stack with the characters
-    for (const char in str) { // O(n)
-        // console.log(str[char]);
-        stack.push(str[char]);
+    for (let i = 0; i < str.length; i++) {
+        stack.push(str[i]);
     }
 
-    // With the LIFO nature of the stack, we pop the characters off of the stack and effectively reverse the string
+    console.log(stack);
+    
     let reversed = '';
-    while (stack.length > 0) { // O(n)
+    while (stack.length > 0) {
         reversed += stack.pop();
     }
 
-    return reversed;
+
+    console.log(reversed);
+
 }
 
-// Approach 2: reversed for loop
-function reverse2(str) {
+function reverse(str) {
     let result = '';
-    for (let i = str.length-1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         result += str[i];
     }
-    
-    console.log(result);
     return result;
 }
 
-// Approach 3: 2-pointers technique
-// Overall time complexity: O(n)
-function reverse3(str) {
-    // since strings are not mutable in JS, we need to turn it into an arr
-    let arr = str.split(''); // O(n)
-    let start = 0; 
-    let end = str.length-1;
 
-    while (start < end) { // O(n)
-        let tmp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = tmp;
-        
-        start++;
-        end--;
-    }
-
-    return arr.join(''); // O(n)
-}
-
-
-let answer = reverse3('hello');
+let answer = reverse2('hello');
 console.log(answer);
 
 
